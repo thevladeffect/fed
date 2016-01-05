27393156e718f6dc2ca781f2336da6b9363e28cb
Vlad Vidac <vladvidac@gmail.com>
2016-01-05 16:59:05 +0200

    minimal layout of food database access interface class

---
+++ app/src/main/java/io/fed/mobile/fedio/Food.java
@@ -0,0 +1,18 @@
+package io.fed.mobile.fedio;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Vlad on 05-Jan-16.
+ */
+public class Food {
+
+    // will return a list of foods matching the @keyword parameter
+    public static ArrayList<String> getResults(String keyword){
+        // format of returned entries in the array list:
+        // NAME_OF_ENTRY***NO_OF_CALORIES_FOR_ONE_DOSE***QUANTITY_IN_ONE_DOSE
+        // string formatting will be done during use
+        return null;
+    }
+
+}
