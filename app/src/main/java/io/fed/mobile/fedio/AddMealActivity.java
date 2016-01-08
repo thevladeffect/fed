c976eab215dedf59dbe07bf7e21b0e1f84da5c6b
ivettczimerman <cz.ivett@yahoo.ro>
2016-01-08 18:27:25 +0200

    Results from query & save items in parse

--- app/src/main/java/io/fed/mobile/fedio/AddMealActivity.java
+++ app/src/main/java/io/fed/mobile/fedio/AddMealActivity.java
@@ -9,16 +9,23 @@ import android.view.Menu;
 import android.view.View;
 import android.app.ActionBar;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SearchView.OnQueryTextListener;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class AddMealActivity extends Activity {
 
     SearchView search;
+    Entry meal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +51,23 @@ public class AddMealActivity extends Activity {
 
             @Override
             public boolean onQueryTextSubmit(String query) {
-                // TODO --- update results list with Entry ArrayList -> Food.getResults(query)
+                // TODO-DONE --- update results list with Entry ArrayList -> Food.getResults(query)
+                ListView lv = (ListView) findViewById(R.id.listView);
+                ArrayList<Entry> results = Food.getResults(query);
+                ArrayAdapter adapter = new ArrayAdapter<String>(AddMealActivity.this,R.id.listView, results);
+                lv.setAdapter(adapter);
 
+                // TODO-DONE --- get selected value from results
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
+                        String selectedFromList =(String) (lv.getItemAtPosition(myItemInt));
+                    }
+                });
 
+                //TODO --- check format of items in ListView and create Entry meal object
+                //meal = new Entry(UUID.randomUUID(),selectedFromList.name, dose, calories per dose);
+                //TextView textView = (TextView)findViewById(R.id.textViewDose);
+                //textView.setText(meal.getDose);
 
 //                Toast.makeText(getBaseContext(), query,
 //                        Toast.LENGTH_SHORT).show();
@@ -66,14 +87,22 @@ public class AddMealActivity extends Activity {
 
     public void buttonOnClick(View v){
 
-        // TODO --- itemId cannot be class field, it needs to be unique across the COMPLETE data set: ex. md5 function on ( itemName + new Date() )
+        Spinner timeOfDay=(Spinner) findViewById(R.id.spinnerTimeOfDay);
+        String timeOfDayTxt = timeOfDay.getSelectedItem().toString();
+        //meal.setTimeOfDay = timeOfDayTxt;
+
+        // TODO - DONE --- itemId cannot be class field, it needs to be unique across the COMPLETE data set: ex. md5 function on ( itemName + new Date() )
 
         // adding data to custom parse table
         // ParseObject userData = new ParseObject("UserData");
-        // userData.put("itemId", );
-        // userData.put("itemName", );
-        // userData.put("createdBy", ParseUser.getCurrentUser().getUsername());
-        // userData.saveInBackground();     // asynchronous adding of data to Parse UserData table
+        // userData.put("itemId", meal.getItemId);
+        // userData.put("itemName", meal.getItemName);
+       //  userData.put("timeOfDay", meal.getTimeOfDay);
+       //  userData.put("dose", meal.getDose);
+       //  userData.put("caloriesPerDose", meal.getCaloriesPerDose);
+
+       //  userData.put("createdBy", ParseUser.getCurrentUser().getUsername());
+       //  userData.saveInBackground();     // asynchronous adding of data to Parse UserData table
 
     }
 
